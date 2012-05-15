c6d5445b67fbfefb655051dbc2e77f347f935fd8
Tyler Cunningham <tyler@cyberchimps.com>
2012-05-14 21:37:37 -0700

    front page template in place, elements need updating

---
+++ front-page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* Front-page template used by Eclipse.
+*
+* Authors: Tyler Cunningham, Trent Lapinski
+* Copyright: © 2012
+* {@link http://cyberchimps.com/ CyberChimps LLC}
+*
+* Released under the terms of the GNU General Public License.
+* You should have received a copy of the GNU General Public License,
+* along with this software. In the main directory, see: /licensing/
+* If not, see: {@link http://www.gnu.org/licenses/}.
+*
+* @package Eclipse.
+* @since 1.0
+*/
+
+	global $options, $themeslug, $post; // call globals
+?>
+
+<?php get_header(); ?>
+
+		<?php
+			foreach(explode(",", $options->get($themeslug.'_front_section_order')) as $fn) {
+				if(function_exists($fn)) {
+					call_user_func_array($fn, array());
+				}
+			}
+		?>
+
+<?php get_footer(); ?>
