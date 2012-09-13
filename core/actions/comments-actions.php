52f119e3d016cced3f3e756d5384fad322170e65
hugosaner <hugo@sanerdesign.com>
2012-09-13 14:59:25 +0100

    changed how translated text is laid out using _e() function instead of echo printf __()
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- core/actions/comments-actions.php
+++ core/actions/comments-actions.php
@@ -93,7 +93,7 @@ function response_comments_loop() {
 	</div>
 
 	<?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
-		<p><?php printf (__( 'You must be', 'response' )); ?><a href="<?php echo wp_login_url( get_permalink() ); ?>"> <?php printf( __( 'logged in', 'response' ), '</a>', __('to post a comment.', 'response' )); ?></p>
+		<p><?php _e( 'You must be ', 'response' ); ?><a href="<?php echo wp_login_url( get_permalink() ); ?>"> <?php _e( 'logged in ', 'response' ); ?></a>, <?php _e('to post a comment.', 'response' ); ?></p>
 	<?php else : ?>
 	
 	<?php comment_form($comments_args); ?>
