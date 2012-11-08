0ca69bef2f0b606da6817fdc3d9d0d9eaf52ab07
hugosaner <hugo@sanerdesign.com>
2012-11-08 16:49:10 +0000

    footer-actions - fixed error in code
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- core/actions/footer-actions.php
+++ core/actions/footer-actions.php
@@ -33,29 +33,29 @@ function response_footer_widgets() {
    	if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Footer") ) { ?>
 		
 		<div class="three columns footer-widgets">
-			<h3 class="footer-widget-title"><?php printf( __( 'Footer Widgets', 'response' )); ?></h3>
+			<h3 class="footer-widget-title"><?php _e( 'Footer Widgets', 'response' ); ?></h3>
 				<p class="footer-text">To customize this widget area login to your admin account, go to Appearance, then Widgets and drag new widgets into Footer Widgets</p>
 		</div>
 
 		<div class="three columns footer-widgets">
-			<h3 class="footer-widget-title"><?php printf( __( 'Recent Posts', 'response' )); ?></h3>
+			<h3 class="footer-widget-title"><?php _e( 'Recent Posts', 'response' ); ?></h3>
 			<ul>
 				<?php wp_get_archives('type=postbypost&limit=4'); ?>
 			</ul>
 		</div>
 
 		<div class="three columns footer-widgets">
-			<h3 class="footer-widget-title"><?php printf( __( 'WordPress', 'response' )); ?></h3>
+			<h3 class="footer-widget-title"><?php _e( 'WordPress', 'response' ); ?></h3>
 			<ul>
     		<?php wp_register(); ?>
     		<li><?php wp_loginout(); ?></li>
-    		<li><a href="<?php echo esc_url( __('http://wordpress.org/', 'response' )); ?>" target="_blank" title="<?php esc_attr_e('Powered by WordPress, state-of-the-art semantic personal publishing platform.', 'response'); ?>"> <?php printf( __('WordPress', 'response' )); ?></a></li>
+    		<li><a href="<?php echo esc_url( __('http://wordpress.org/', 'response' )); ?>" target="_blank" title="<?php esc_attr_e('Powered by WordPress, state-of-the-art semantic personal publishing platform.', 'response'); ?>"> <?php _e('WordPress', 'response' ); ?></a></li>
     		<?php wp_meta(); ?>
     		</ul>
 		</div>
 		
 		<div class="three columns footer-widgets">
-			<h3 class="footer-widget-title"><?php printf( __( 'Search', 'response' )); ?></h3>
+			<h3 class="footer-widget-title"><?php _e( 'Search', 'response' ); ?></h3>
 			<?php get_search_form(); ?>
 		</div>
 		
