5e5ca5ed2f62c9c4ecd3b4ad1b025614c95c2093
hugosaner <hugo@sanerdesign.com>
2013-01-18 18:07:46 +0000

    Changed printf call to _e()
    Signed-off-by: hugosaner <hugo@sanerdesign.com>

--- searchform.php
+++ searchform.php
@@ -19,6 +19,6 @@
 
 <form method="get" class="searchform" action="<?php echo home_url(); ?>/">
 	<div id="magnify"><img src="<?php echo get_template_directory_uri() ;?>/images/magnify.png" alt="magnify" /></div>
-	<div><input type="text" name="s" class="s" value="<?php printf( __( 'Search', 'response' )); ?>" id="searchsubmit" onfocus="if (this.value == 'Search') this.value = '';" /></div>
+	<div><input type="text" name="s" class="s" value="<?php _e( 'Search', 'response' ); ?>" id="searchsubmit" onfocus="if (this.value == 'Search') this.value = '';" /></div>
 	<div><input type="submit" class="searchsubmit" value="" /></div>
 </form>
